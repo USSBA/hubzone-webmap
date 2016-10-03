9a480d7a2aecaec32d3328d404be0715b739b0ee
Doug Price <dprice@fearless.tech>
2016-10-03 11:08:50 -0400

    imports uswds v0.12.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,13 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+%w(js css img fonts).each do |p|
+  Rails.application.config.assets.paths <<
+    Rails.root.join("vendor", "assets", "uswds-0.12.1", p)
+end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( uswds.js )
+Rails.application.config.assets.precompile += %w( uswds.css )
