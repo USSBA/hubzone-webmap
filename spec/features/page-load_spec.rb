fa5da53ce15b4a408f0519ce064e8966990a0d7f
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-10-28 15:32:33 -0400

    small update

--- spec/features/page-load_spec.rb
+++ spec/features/page-load_spec.rb
@@ -4,10 +4,10 @@ describe "the page loads", type: :feature do
   it "goes to the homepage" do
     visit('/')
   end
-  it "should have header" do
+  it "should have header section" do
     expect(page).to have_selector('header')
   end
-  it "should have body" do
+  it "should have main section" do
     expect(page).to have_selector('body')
   end
   it "should have a map div" do
