e974aa690d979dc4122a7ce3a57b8712124efbe4
Tiffay Forkner <thetif@gmail.com>
2016-10-28 17:35:23 -0400

    removed unnecessary method

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,8 +2,4 @@ class MapController < ApplicationController
   def fake
     render layout: false
   end
-
-  def googleMapStyleConfig
-    render file: "/config/google-maps-style-config.json"
-  end
 end
