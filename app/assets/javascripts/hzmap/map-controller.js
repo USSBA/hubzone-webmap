2e7b6adddf8485b71de32df0bb48c8d91c6c6d2f
Jonathan <jon.dandois@gmail.com>
2016-10-28 12:17:25 -0400

    page loads and has a simple basemap with no special style

--- app/assets/javascripts/hzmap/map-controller.js
+++ app/assets/javascripts/hzmap/map-controller.js
@@ -1,1 +1,18 @@
 console.log("In the map controller");
+
+
+//create the map on load and handle styling, build in event listeners
+function initMap() {
+  // Styles a map
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.5, lng: -97.5},
+    zoom: 9,
+    zoomControl: true, 
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.TOP_RIGHT
+    }
+  });
+
+  return map;
+
+};
