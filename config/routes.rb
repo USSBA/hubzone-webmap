9e3b485657c857507def7fc9d4b63e40f8c99a12
Tiffay Forkner <thetif@gmail.com>
2016-10-28 13:21:12 -0400

    first capybara test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'map/fake'
 
-  root 'map#fake'
+  root :to => 'map#fake'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -2,5 +2,6 @@ describe "index page", :type => :feature do
   it "goes to the home page" do
     visit root_path
     expect(page).to have_content 'Geocoding Results'
+    expect(page).to have_content 'Enter a query to geocode'
   end
 end
