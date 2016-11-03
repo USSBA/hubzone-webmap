fb2c505342b44a8b91a9ed1c084e79aa429ffa6b
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-11-03 16:11:30 -0400

    modified test spec for sidebar

--- spec/features/page_sidebar_spec.rb
+++ spec/features/page_sidebar_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
-describe "the sidebar should load and have a button", type: :feature do
+describe "the sidebar should load", type: :feature do
   before do
     visit('/map')
   end
   it "should have a sidebar" do
-    expect(page).to have_selector(:css, ".map-sidebar")
-  end
-  it "should have a sidebar button" do
-    expect(page).to have_selector(:css, ".map-sidebar-button")
+    expect(page).to have_selector(:css, "#sidebar")
   end
 end
