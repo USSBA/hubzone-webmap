bfa713f669b3bec9799bd4bbe3211868af32b728
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-11-07 08:55:31 -0500

    added json return object to map_controller.rb and tweaked page_header_spec

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,4 +5,28 @@ class MapController < ApplicationController
   # end
   def index
   end
+
+  def search
+    case params[:q]
+    when "foo"
+      value = { search: "foo", address: "8 Market Place, Baltimore, MD  21202", lat: 36, lon: -76,
+                hubzone: [ { type: "indian_lands",
+                             indian: "apache",
+                             start: "1950-07-10" },
+                            { type: "qct",
+                              tract: "1283129873",
+                              start: "1999-08-30",
+                              stop: "2017-04-01" }] }
+      stat = :success
+    when "bar"
+      value = { search: "bar", address: "2403 Pelham Ave", lat: 36, lon: -76,
+                hubzone: [] }
+      stat = :success
+    else
+      value = { search: "wtf", address: "road to nowhere", lat: 36, lon: -76,
+                hubzone: [] }
+      stat = :not_found
+    end
+    render json: value.to_json, status: stat
+  end
 end
