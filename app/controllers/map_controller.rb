9f292c869f08787e3b6f1de630a2e2c8a0b40b1a
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-11-07 11:38:11 -0500

    added Excon, updated map_controller to use hubzone-api

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,6 @@
 # Provides access to the main page with the HUBZone map
+require 'excon'
+
 class MapController < ApplicationController
   # def fake
   #   render :layout => false
@@ -7,26 +9,9 @@ class MapController < ApplicationController
   end
 
   def search
-    case params[:q]
-    when "foo"
-      value = { search: "foo", address: "8 Market Place, Baltimore, MD  21202", lat: 36, lon: -76,
-                hubzone: [ { type: "indian_lands",
-                             indian: "apache",
-                             start: "1950-07-10" },
-                           { type: "qct",
-                             tract: "1283129873",
-                             start: "1999-08-30",
-                             stop: "2017-04-01" }] }
-      stat = :success
-    when "bar"
-      value = { search: "bar", address: "2403 Pelham Ave", lat: 36, lon: -76,
-                hubzone: [] }
-      stat = :success
-    else
-      value = { search: "wtf", address: "road to nowhere", lat: 36, lon: -76,
-                hubzone: [] }
-      stat = :not_found
-    end
-    render json: value.to_json, status: stat
+    query = params[:q]
+    response = Excon.get('http://localhost:3001/search?q=' + query)
+    status = response.status
+    render json: response.body, status: status
   end
 end
