01a5ee8c469b927b70d48cfe10b4e171ac57cd1d
Doug Price <dprice@fearless.tech>
2016-11-11 12:09:35 -0500

    US1755: stubbed out the Excon connection for tests against the geo api.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -11,11 +11,26 @@ class MapController < ApplicationController
 
   def search
     query = URI.encode_www_form("q" => params[:search] ||= ' ')
-    response = Excon.get("#{MAP_CONFIG[:hubzone_api_host]}/search?#{query}", expects: [200])
+    response = connection.request(method: :get, path: "/search?#{query}")
     @body = response.data[:body]
     respond_to do |format|
       format.html {}
       format.js {}
     end
+  end
+
+  private
+
+  def connection
+    if Rails.env == 'test'
+      mocked_connection
+    else
+      Excon.new(MAP_CONFIG[:hubzone_api_host])
+    end
+  end
+
+  def mocked_connection
+    Excon.new(MAP_CONFIG[:hubzone_api_host],
+              mock: true, allow_unstubbed_requests: true)
   end
 end
