b4f30c7a1705b9f54d6620b861c1fc84f8e33f15
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-11-15 08:44:46 -0500

    hub-33 update map style ux

--- app/assets/javascripts/hzmap/google-map-style-config.js
+++ app/assets/javascripts/hzmap/google-map-style-config.js
@@ -1,76 +1,224 @@
-var googleMapsStyleConfig = [{
-  "featureType": "landscape",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "lightness": 65
-  }, {
-    "visibility": "on"
-  }]
-}, {
-  "featureType": "poi",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "lightness": 51
-  }, {
-    "visibility": "simplified"
-  }]
-}, {
-  "featureType": "road.highway",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "visibility": "simplified"
-  }]
-}, {
-  "featureType": "road.arterial",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "lightness": 30
-  }, {
-    "visibility": "on"
-  }]
-}, {
-  "featureType": "road.local",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "lightness": 40
-  }, {
-    "visibility": "on"
-  }]
-}, {
-  "featureType": "transit",
-  "stylers": [{
-    "saturation": -100
-  }, {
-    "visibility": "simplified"
-  }]
-}, {
-  "featureType": "administrative.province",
-  "stylers": [{
-    "visibility": "off"
-  }]
-}, {
-  "featureType": "water",
-  "elementType": "labels",
-  "stylers": [{
-    "visibility": "on"
-  }, {
-    "lightness": -25
-  }, {
-    "saturation": -100
-  }]
-}, {
-  "featureType": "water",
-  "elementType": "geometry",
-  "stylers": [{
-    "hue": "#ffff00"
-  }, {
-    "lightness": -25
-  }, {
-    "saturation": -97
-  }]
-}];
+var googleMapsStyleConfig = [
+    {
+        "featureType": "administrative.province",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-82"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "hue": "#ff0000"
+            },
+            {
+                "saturation": "-0"
+            },
+            {
+                "lightness": "20"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#0071ff"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51
+            },
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#e7f4e3"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            },
+            {
+                "color": "#aeb0b5"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    }
+]
