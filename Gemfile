e0d17bbda1e73126f7490849918ff5b015d5f432
Tiffay Forkner <thetif@gmail.com>
2016-11-23 09:36:08 -0700

    tests working

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ group :development, :test do
   gem 'rubocop' # Enforce ruby code style
   gem 'selenium-webdriver'
   gem 'simplecov', require: false # determine code coverage of tests
-  gem 'teaspoon'
   gem 'teaspoon-jasmine'
 end
 
--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -10,7 +10,7 @@ Teaspoon.configure do |config|
 
   # Paths that will be appended to the Rails assets paths
   # Note: Relative to `config.root`.
-  config.asset_paths = ["app/assets/javascripts", "app/assets/stylesheets"]
+  config.asset_paths = ["spec/javascripts", "spec/stylesheets", "app/assets/javascripts", "app/assets/stylesheets"]
 
   # Fixtures are rendered through a controller, which allows using HAML, RABL/JBuilder, etc. Files in these paths will
   # be rendered as fixtures.
@@ -38,7 +38,8 @@ Teaspoon.configure do |config|
 
     # Specify a file matcher as a regular expression and all matching files will be loaded when the suite is run. These
     # files need to be within an asset path. You can add asset paths using the `config.asset_paths`.
-    suite.matcher = "spec/javascripts/**/*.{js,js.coffee,coffee}"
+    #suite.matcher = "spec/javascripts/**/*.{js,js.coffee,coffee}"
+    suite.matcher = "{spec/javascripts,app/assets/javascripts}/**/*.{js,js.coffee,coffee}"
 
     # Load additional JS files, but requiring them in your spec helper is the preferred way to do this.
     # suite.javascripts = []
