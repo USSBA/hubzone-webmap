6f8b9eb5cb7bad3f473bb8e22f8f503d8c8c0027
Tyler Bolchoz <tbolchoz@fearless.tech>
2016-12-05 08:49:12 -0500

    HUB-87:improving code via jshint

--- app/assets/javascripts/hzmap/sidebar.js
+++ app/assets/javascripts/hzmap/sidebar.js
@@ -7,24 +7,20 @@
 $.fn.sidebar = function() {
   var $sidebar = this;
   var $container = $('#sidebar');
-  this.currentClass = 'hidden';
 
   this.update = function() {
     if (!$sidebar.hasClass('on')) {
       $sidebar.addClass('on');
       $sidebar.removeClass('hidden');
       $('#sidebar-button').html('<i class="fa fa-chevron-right"></i>');
-      this.currentClass = 'on';
     } else {
-      $sidebar.addClass('hidden');
       $sidebar.removeClass('on');
       $('#sidebar-button').html('<i class="fa fa-chevron-left"></i>');
-      this.currentClass = 'hidden';
     }
   };
   /*** Open the sidebar ***/
   this.open = function() {
-    if ($sidebar.hasClass('hidden')) {
+    if (!$sidebar.hasClass('on')) {
       $sidebar.update();
     }
   };
