cd89598f4faa6b8fd6dc65ad4232dbb3704d8b9d
Jonathan <jon.dandois@gmail.com>
2016-12-09 08:48:46 -0500

    HUB-126 testing WMS as tiles

--- app/assets/javascripts/hzmap/map.js
+++ app/assets/javascripts/hzmap/map.js
@@ -20,9 +20,11 @@ function initMap() {
   //adds the map legend
   map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(document.getElementById('legend'));
 
-  var newHZOverlay = generateHZOverlay();
+  // var newHZCurrentOverlay = generateHZOverlay('hz_current');
+  // var newHZILOverlay = generateHZOverlay('indian_lands');
 
-  map.overlayMapTypes.push(newHZOverlay);
+  map.overlayMapTypes.push(generateHZOverlay('hz_current'));
+  map.overlayMapTypes.push(generateHZOverlay('indian_lands'));
 
   // adds listener that triggers whenever the map is idle to update with new features.
   // google.maps.event.addListener(map, 'idle', function(){
@@ -176,60 +178,40 @@ function catchMapClick(clickEvent){
   return url;
 }
 
-function generateHZOverlay(){
+function generateHZOverlay(layer){
   return new google.maps.ImageMapType({
+
               getTileUrl: function (coord, zoom) {
-                  var proj = map.getProjection();
-                  var zfactor = Math.pow(2, zoom);
-                  // get Long Lat coordinates
-                  var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
-                  var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
+                console.log('here', coord);
 
-                  //corrections for the slight shift of the SLP (mapserver)
-                  var deltaX = 0;
-                  var deltaY = 0;
+                var proj = map.getProjection();
+                var zfactor = Math.pow(2, zoom);
+                // get Long Lat coordinates
+                var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor));
+                var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor));
 
-                  //create the Bounding box string
-                  var bbox =     (top.lng() + deltaX) + "," +
-                               (bot.lat() + deltaY) + "," +
-                               (bot.lng() + deltaX) + "," +
-                               (top.lat() + deltaY);
+                //create the Bounding box string
+                var bbox = top.lng() + "," +
+                           bot.lat() + "," +
+                           bot.lng() + "," +
+                           top.lat();
 
-                  //base WMS URL
-                  var url = "http://localhost:8080/geoserver/hubzone-test/wms?service=WMS";
-                  url += "&REQUEST=GetMap"; //WMS operation
-                  url += "&SERVICE=WMS";    //WMS service
-                  url += "&VERSION=1.1.0";  //WMS version  
-                  url += "&LAYERS=" + "hubzone-test:hz_current"; //WMS layers
-                  url += "&FORMAT=image/png" ; //WMS format
-                  // url += "&BGCOLOR=0xFFFFFF";  
-                  url += "&TRANSPARENT=TRUE";
-                  url += "&SRS=EPSG:4326";     //set WGS84 
-                  url += "&BBOX=" + bbox;      // set bounding box
-                  url += "&WIDTH=256";         //tile size in google
-                  url += "&HEIGHT=256";
-                  return url;                 // return URL for the tile
+                //base WMS URL
+                var url = "http://localhost:8080/geoserver/hubzone-test/wms?service=WMS";
+                url += "&REQUEST=GetMap"; //WMS operation
+                url += "&SERVICE=WMS";    //WMS service
+                url += "&VERSION=1.1.0";  //WMS version  
+                url += "&LAYERS=" + "hubzone-test:" + layer; //WMS layers
+                url += "&FORMAT=image/png" ; //WMS format
+                url += "&TRANSPARENT=TRUE";
+                url += "&SRS=EPSG:4326";     //set WGS84 
+                url += "&BBOX=" + bbox;      // set bounding box
+                url += "&WIDTH=256";         //tile size in google
+                url += "&HEIGHT=256";
+                return url;                 // return URL for the tile
 
               },
               tileSize: new google.maps.Size(256, 256),
               isPng: true,
-              // opacity: 0.5
           }); 
 }
-
-
-
-//timers for testing only
-var docLoaded = null;
-var windowReady = null;
-
-$( document ).ready(function() {
-  docLoaded = Date.now();
-  console.log( "document loaded" );
-});
-
-$( window ).load(function() {
-  windowReady = Date.now();
-  console.log( "window loaded" );
-  console.log( (windowReady - docLoaded)/1000 +  " seconds" );
-});
