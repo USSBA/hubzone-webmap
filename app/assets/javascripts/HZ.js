161008138b58d555ce064df57099311d4ad098e3
Doug Price <dprice@fearless.tech>
2016-12-16 16:41:38 -0500

    HUB-121: Adds Google Analytics to the map

---
+++ app/assets/javascripts/HZ.js
@@ -0,0 +1,36 @@
+/* exported HZ */
+var HZ = (function() {
+
+  window.console = window.console || { };
+  if ( window.console.log == null ) {
+    window.console.log = function() { };
+  }
+
+  return {
+    openLink : function( url, category, action, label, value ) {
+      console.log( "HZ.openLink: ", url, category, action, label, value );
+      if ( typeof ga === "function" ) {
+        var timeout = setTimeout( function() { document.location = url; }, 500 );
+        ga( 'send', 'event', category || "outbound", action || "click", label || url, value, {
+          hitCallback : function() {
+            clearTimeout( timeout );
+            document.location = url;
+          }
+        });
+      } else {
+        document.location = url;
+      }
+    },
+    track : function( category, action, label, value ) {
+      console.log( "HZ.track: ", category, action, label, value );
+      if ( typeof ga === "function" ) {
+        ga( "send", "event", category, action, label, value );
+      }
+    },
+    trackSubmit : function(action, inputId) {
+      var term = $(inputId).val();
+      this.track( 'map', action, term );
+    }
+  };
+})();
+
