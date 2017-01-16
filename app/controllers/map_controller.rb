58776f44239d2b85605bfadff92da1799644ddf8
Doug Price <dprice@fearless.tech>
2017-01-16 15:47:54 -0500

    HUB-128: root path redirects to map url

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,9 +3,6 @@ require 'uri'
 
 # Provides access to the main page with the HUBZone map
 class MapController < ApplicationController
-  # def fake
-  #   render :layout => false
-  # end
   def index
   end
 
