fec832dccde39d059bef8eceecc6b61d97656843
Tyler Bolchoz <tbolchoz@fearless.tech>
2017-01-27 11:45:56 -0500

    HUB-209:typeahead behavior updated

--- app/assets/javascripts/hzmap/autocomplete.js
+++ app/assets/javascripts/hzmap/autocomplete.js
@@ -5,9 +5,15 @@ HZApp.Autocomplete = {
   },
   options: {
     types: []
-  }, 
+  },
   autocomplete: {},
   createAutocomplete: function(){
   	this.autocomplete = new google.maps.places.Autocomplete(this.searchInput(), this.options);
+    this.createListener();
+  },
+  createListener: function(){
+    HZApp.Autocomplete.autocomplete.addListener('place_changed', function() {
+      $('.usa-search').submit();
+    });
   }
 };
