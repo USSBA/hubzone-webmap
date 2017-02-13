74da7c64bb6f47c2475023a6a041ab5f7718f339
Tyler Bolchoz <tbolchoz@fearless.tech>
2017-02-13 10:20:17 -0500

    HUB-46:refactor legend defs

---
+++ app/assets/javascripts/hzmap/legend-defs.js
@@ -0,0 +1,93 @@
+// hubzone data layer style definitions
+
+// this object holds the current google overlays in the .overlay array and the per layer styles
+// order in this object defines draw order on the map:
+// first object is drawn first, then next on top of that, etc.
+HZApp.Legend.LegendDefs = (function(){
+  var legendDefaults = {
+    circleFillColor: '#FFFFFF',
+    circleFillOpacity: 0.5,
+    circleStrokeColor: '#CCCCCC',
+    circleStrokeOpacity: 1,
+    circleStrokeWidth: 1,
+    displacementX: 0,
+    displacementY: 0,
+    fillColor: '#CCCCCC',
+    fillOpacity: 0.5,
+    graphicSpacing: 10,
+    lineStrokeColor: '#000000',
+    lineStrokeOpacity: 1,
+    lineStrokeWidth: 1,
+    lineRotation: 0,
+    strokeColor: '#CCCCCC',
+    strokeOpacity: 1,
+    strokeWidth: 1.25,
+    tileSize: 10,
+
+    // USWDS Alt 1
+    qctColor: '#0D465C',
+    qnmcColor: '#BA233F',
+    indianLandsColor: '#009DCD'
+  };
+
+  var legendKeys = {
+      indian_lands: {
+        styleOptions: [
+          {
+            type: 'polygon',
+            fillColor: legendDefaults.indianLandsColor,
+            fillOpacity: legendDefaults.fillOpacity,
+            strokeColor: legendDefaults.indianLandsColor,
+            strokeOpacity: legendDefaults.strokeOpacity,
+            strokeWidth: legendDefaults.strokeWidth
+          }
+        ]
+      },
+      qnmc: {
+        styleOptions: [
+          {
+            type: 'polygon',
+            fillColor: legendDefaults.qnmcColor,
+            fillOpacity: legendDefaults.fillOpacity,
+            strokeColor: legendDefaults.qnmcColors,
+            strokeOpacity: legendDefaults.strokeOpacity,
+            strokeWidth: legendDefaults.strokeWidth
+          }
+        ]
+      },
+      qct: {
+        styleOptions: [
+          {
+            type: 'polygon',
+            fillColor: legendDefaults.qctColor,
+            fillOpacity: legendDefaults.fillOpacity,
+            strokeColor: legendDefaults.qctColor,
+            strokeOpacity: legendDefaults.strokeOpacity,
+            strokeWidth: legendDefaults.strokeWidth
+          }
+        ]
+      },
+      brac: {
+        styleOptions: [
+          {
+            type: 'circle',
+            circleFillColor: '#000',
+            circleFillOpacity: legendDefaults.fillOpacity,
+            circleStrokeColor: '#000000',
+            circleStrokeOpacity: legendDefaults.strokeOpacity,
+            circleStrokeWidth: legendDefaults.strokeWidth,
+            strokeColor: '#000000',
+            strokeOpacity: legendDefaults.strokeOpacity,
+            strokeWidth: legendDefaults.strokeWidth,
+            tileSize: 15,
+            graphicSpacing: legendDefaults.graphicSpacing
+          }
+        ]
+      },
+    };
+
+  return {
+    legendDefaults: legendDefaults,
+    legendKeys: legendKeys
+  };
+})();
