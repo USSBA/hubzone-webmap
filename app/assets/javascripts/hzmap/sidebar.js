60415a7ba3dee9ff0a7fcb1a6dbd2716f63c0e4c
Tyler Bolchoz <tbolchoz@fearless.tech>
2017-02-14 09:16:24 -0500

    HUB-46:removing mobile code for another branch

--- app/assets/javascripts/hzmap/sidebar.js
+++ app/assets/javascripts/hzmap/sidebar.js
@@ -11,7 +11,6 @@ $.fn.sidebar = function() {
   this.update = function() {
     if (!$sidebar.hasClass('on')) {
       $sidebar.addClass('on');
-      $('#legend').addClass('legend-sidebar');
       $sidebar.removeClass('hidden');
       $('#sidebar-button').html('<i class="fa fa-chevron-right"></i>');
       $('div.gmnoprint[controlheight="55"], div.gmnoprint[controlheight="66"], .gm-svpc').addClass('gm-sidebar-on');
@@ -19,7 +18,6 @@ $.fn.sidebar = function() {
       this.currentClass = 'on';
     } else {
       $sidebar.removeClass('on');
-      $('#legend').removeClass('legend-sidebar');
       $('#sidebar-button').html('<i class="fa fa-chevron-left"></i>');
       $('div.gmnoprint[controlheight="55"], div.gmnoprint[controlheight="66"], .gm-svpc').removeClass('gm-sidebar-on');
       $('#geolocation').removeClass('geolocation-sidebar-on');
