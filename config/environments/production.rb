cfa6a4bdcb6ceaa8266aa389f91915163788fd65
Tyler Bolchoz <tybolt@gmail.com>
2017-03-15 10:30:40 -0400

    HUB-287:i18n

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,9 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  
+  # Enable multiple locale files to be loaded
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
