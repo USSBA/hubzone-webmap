788eed08e3e7862e67be6e38b927a81d17d3d7e0
Tyler Bolchoz <tybolt@gmail.com>
2017-03-31 14:30:55 -0400

    HUB-375:integration testing

---
+++ spec/features/map_legend_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe "The Legend", type: :feature do
+  before do
+    visit('/map')
+  end
+  it "should have a title" do
+    expect(page).to have_content('Legend')
+  end
+  it "should have a qualification title" do
+    expect(page).to have_content('Qualified HUBZones')
+  end
+  it "should have a expiring title" do
+    expect(page).to have_content('Expiring HUBZones')
+  end
+  it "should have title, toggle, and symbol(s) for qct" do
+    expect(page).to have_content('Census Tract')
+    expect(page).to have_selector('input#qct')
+    expect(page).to have_css('.layer-qct')
+  end
+  it "should have title, toggle, and symbol(s) for qnmc" do
+    expect(page).to have_content('County')
+    expect(page).to have_selector('input#qnmc')
+    expect(page).to have_css('.layer-qnmc')
+  end
+  it "should have title, toggle, and symbol(s) for indian_lands" do
+    expect(page).to have_content('Indian Land')
+    expect(page).to have_selector('input#indian_lands')
+    expect(page).to have_css('.layer-indian_lands')
+  end
+  it "should have title, toggle, and symbol(s) for redesignated" do
+    expect(page).to have_content('Redesignated')
+    expect(page).to have_selector('input#redesignated')
+    expect(page).to have_css('.layer-qct_r')
+    expect(page).to have_css('.layer-qnmc_r')
+  end
+  it "should have title, toggle, and symbol(s) for brac" do
+    expect(page).to have_content('Closed Base Area')
+    expect(page).to have_selector('input#brac')
+    expect(page).to have_css('.layer-qct_b')
+    expect(page).to have_css('.layer-qnmc_brac')
+    expect(page).to have_css('.layer-brac')
+  end
+end
