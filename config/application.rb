75754c1aa8be54a6157a08d8287e5561807febbc
Jonathan <jon.dandois@gmail.com>
2017-05-05 13:51:35 -0400

    upgrading to rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,11 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module HubzoneMap
+  # the application
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults 5.1
   end
 end
