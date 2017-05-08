18fe466daa8e330a6cefe356830eb15917b58e78
PatKelsh <pkelsh@fearless.tech>
2017-05-08 14:50:14 -0400

    HUB-288 used constant for supported locales based on pull request feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    locales = %w(en dev)
-    I18n.locale = if locales.include? params[:locale]
+    return I18n.locale = I18n.default_locale if params[:locale].nil?
+    locales = I18n.available_locales
+    I18n.locale = if locales.include? params[:locale].to_sym
                     params[:locale]
                   else
                     I18n.default_locale
