ae61230c7822412ca76db22076d6034b45de00db
Jonathan <jon.dandois@gmail.com>
2017-06-23 14:26:03 -0400

    HUB-214 add in the regex method for getting a new location.hash string based on a new parameter

---
+++ app/assets/javascripts/hzmap/router.js
@@ -0,0 +1,26 @@
+// Basic hash router module
+HZApp.Router = (function(){
+  return {
+
+    // returns a new hash string that
+    updateHashValue: function(hashValue, hashParam){
+      var newHash = hashParam + "=" + hashValue;
+      var updatedHash = "";
+      var hashParamRegex = new RegExp(hashParam + "=", "ig");
+      var hashRegexInside = new RegExp(hashParam + "=" + '.+?(?=\&)', "ig"); //if the hash is between # and &
+      var hashRegexOutside = new RegExp(hashParam + "=" + '.+?(?=$)', "ig"); // if the hash is between (# and end of line) or (& and end of line)
+
+      if (location.hash === "") {
+        updatedHash = newHash;
+      } else if (location.hash.match(hashParamRegex) === null) {
+        updatedHash = location.hash + "&" + newHash;
+      } else if (location.hash.match(hashRegexInside) !== null) {
+        updatedHash = location.hash.replace(hashRegexInside, newHash);
+      } else if (location.hash.match(hashRegexOutside) !== null) {
+        updatedHash = location.hash.replace(hashRegexOutside, newHash);
+      }
+      return updatedHash;
+    },
+
+  };
+})();
--- spec/javascripts/hzmap/router_spec.js
+++ spec/javascripts/hzmap/router_spec.js
@@ -13,11 +13,41 @@ describe ('Testing Router operations', function() {
   afterEach(function(){
     google = {};
     HZApp.map = {};
+    location.hash = "";
   });
 
   describe("adding app data to the URL hash", function(){
-    it("should add map center to the hash", function(){
 
+    describe("updating map center", function(){
+      var center, centerValue;
+      beforeEach(function(){
+        center = HZApp.map.getCenter();
+        centerValue = center.lat() + "," + center.lng();
+      });
+
+      it("should add map center to the hash when hash is empty", function(){
+        var updatedHash = HZApp.Router.updateHashValue(centerValue, "center");
+        expect(updatedHash).toEqual("center=" + centerValue);
+      });
+
+      it("should add map center when hash is not empty but doesnt have center", function(){
+        location.hash = "foo=bar";
+        var updatedHash = HZApp.Router.updateHashValue(centerValue, "center");
+        expect(updatedHash).toEqual(location.hash + "&center=" + centerValue);
+      });
+
+      it("should update map center when center is present in middle of hash", function(){
+        location.hash = "foo=bar&center=39,-76&zoom=15";
+        var updatedHash = HZApp.Router.updateHashValue(centerValue, "center");
+        expect(updatedHash).toEqual("#foo=bar&center=" + centerValue + "&zoom=15");
+      });
+
+      it("should update map center when center is present at end of hash", function(){
+        location.hash = "foo=bar&zoom=15&center=39,-76";
+        var updatedHash = HZApp.Router.updateHashValue(centerValue, "center");
+        expect(updatedHash).toEqual("#foo=bar&zoom=15&center=" + centerValue);
+      });
     });
+
   });
 });
