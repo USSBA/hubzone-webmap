2383b299bae9eb09fbc9ffa75370868c8bdb3425
Tyler Bolchoz <tybolt@gmail.com>
2017-06-27 17:14:44 -0400

    HUB-509 figuring out testing

--- spec/features/map_search_spec.rb
+++ spec/features/map_search_spec.rb
@@ -95,13 +95,74 @@ RSpec.describe 'The Search', type: :feature, js: true do
         http_status: 200,
         hubzone: [
           {
-            hz_type: "indian_lands",
-            expires: nil
-
+            hz_type: "qct",
+            expires: nil,
+            expires: Date.tomorrow,
+            tract_fips: "21203950400",
+            county: "Rockcastle County",
+            state: "KY"
           },
           {
-            hz_type: "qct",
-            expires: nil
+            hz_type: "qnmc",
+            expires: Date.tomorrow,
+            county_fips: "21203950409",
+            county: "Harford County",
+            state: "MD"
+          },
+          {
+            hz_type: "brac",
+            brac_sba_name: "Central Base",
+            fac_type: "Base",
+            effective: Date.today
+          },
+          {
+            hz_type: "qct_brac",
+            brac_sba_name: "Central Base",
+            fac_type: "Base",
+            effective: Date.today,
+            tract_fips: "21203950400",
+            county_name: "Rockcastle County",
+            state: "KY"
+          },
+          {
+            hz_type: "qnmc_brac",
+            brac_sba_name: "North Base",
+            fac_type: "Closed Base",
+            effective: Date.today,
+            county_fips: "21203950400",
+            county_name: "Montgomery County",
+            state: "MD"
+          },
+          {
+            hz_type: "qct_qda",
+            incident_description: "Tornado",
+            qda_declaration: Date.today,
+            qda_designation: Date.today,
+            qda_publish: Date.today,
+            expires: Date.tomorrow,
+            tract_fips: "21203950400",
+            county_name: "Rockcastle County",
+            state: "KY"
+          },
+          {
+            hz_type: "qnmc_qda",
+            incident_description: "Tornado and Earthquake",
+            qda_declaration: Date.today,
+            qda_designation: Date.today,
+            qda_publish: Date.today,
+            expires: Date.tomorrow,
+            county_fips: "21203950410",
+            county_name: "Murky County",
+            state: "KY"
+          },
+          {
+            hz_type: "indian_lands",
+            expires: nil,
+            name: "Navajo Nation AZ",
+            census: "042430R",
+            type: "Reservation",
+            class: "American Indian Area",
+            gnis: "42851"
           }
         ],
         geometry: {
@@ -162,17 +223,34 @@ RSpec.describe 'The Search', type: :feature, js: true do
               it "should have the right layer symbology" do
                 expect(page).to have_css(".layer-" + tquery[:response][:hubzone][0][:hz_type])
               end
-              next unless hubzone[:expires]
-              it "should show the correct language for expires or expired if expiration date is present" do
-                expect(page).to have_content(hubzone[:expires] < Date.today ? t('hubzone_assertions.expired') : t('hubzone_assertions.expires'))
-              end
-              next unless hubzone[:name].nil?
+              # todo: change this to be 1 it per hubzone designation
+              next unless tquery[:search] == 'tiffany peak, co'
+              puts 'Hi just tiffanty peak'
+              next unless tquery[:response][:hubzone][0][:hz_type] == 'indian_lands'
               it "should show additional data fields" do
+                # byebug
+                puts 'hi, only searching fiffany peak, co brac'
+                expect(page).to have_content(hubzone[:brac_sba_name])
+                expect(page).to have_content(hubzone[:fac_type])
+                expect(page).to have_content(hubzone[:effective])
                 expect(page).to have_content(hubzone[:name])
                 expect(page).to have_content(hubzone[:census])
                 expect(page).to have_content(hubzone[:type])
                 expect(page).to have_content(hubzone[:class])
                 expect(page).to have_content(hubzone[:gnis])
+                expect(page).to have_content(hubzone[:tract_fips])
+                expect(page).to have_content(hubzone[:county])
+                expect(page).to have_content(hubzone[:county_name])
+                expect(page).to have_content(hubzone[:state])
+                expect(page).to have_content(hubzone[:county_fips])
+                expect(page).to have_content(hubzone[:incident_description])
+                expect(page).to have_content(hubzone[:qda_declaration])
+                expect(page).to have_content(hubzone[:qda_designation])
+                expect(page).to have_content(hubzone[:qda_publish])
+              end
+              next unless hubzone[:expires]
+              it "should show the correct language for expires or expired if expiration date is present" do
+                expect(page).to have_content(hubzone[:expires] < Date.today ? t('hubzone_assertions.expired') : t('hubzone_assertions.expires'))
               end
             end
           end
