1a0a50f9836a82b88e3101da24c1e72957e6f5d1
Pat <pat@Pats-MacBook-Pro.local>
2017-09-19 13:38:59 -0400

    HUB-799 removed excess lines

--- spec/javascripts/hzmap/sidebar_spec.js
+++ spec/javascripts/hzmap/sidebar_spec.js
@@ -148,8 +148,6 @@ describe ('Testing sidebar operations', function() {
 
   it ("should recenter the map on address-marker click to the query geocode when provided", function(){
     location.hash = "#center=45.493490,-98.249910&zoom=5&q=40.813809,-102.172852";
-    // mock response of geocode location
-    spyOn(HZApp.HZQuery.response, 'geocodeLocation').and.returnValue({lat: 12.00, lng: 39.00});
     spyOn(HZApp.Router, "updateCenter");
     HZApp.SidebarUtils.centerMapMarker({q: "40.813809,-102.172852"}, {lat: 40.813809, lng: -102.172852});
     expect(HZApp.Router.updateCenter.calls.count()).toEqual(1);
