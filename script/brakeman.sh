062a6c4b3fec4120003ed0ea836a7d9d034a8eb9
Lee Place <leeplace@Lees-MacBook-Pro.local>
2018-01-18 15:03:41 -0500

    HUB-416 added brakeman

--- script/brakeman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Script for running Brakeman tests
-# Brakeman is a security scanner https://github.com/presidentbeef/brakeman.
-
-gem install --no-rdoc --no-ri brakeman
-brakeman --exit-on-warn .
