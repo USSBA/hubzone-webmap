3a46e23bf41196d6c1a44d1f3d243c68fa5dc5a7
Charles Bushong <cbushong@fearless.tech>
2018-04-10 15:44:58 -0400

    HUB-1137: Adding stdout to dev environment

--- config/environments/dev.rb
+++ config/environments/dev.rb
@@ -6,4 +6,10 @@ Rails.application.configure do
   config.assets.compile = false
   config.assets.debug = false
   config.assets.digest = true
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
