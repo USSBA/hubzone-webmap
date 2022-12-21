1a5e81b3d5985da95b27e062f641be4cd58cdcc4
Ryan Whorton <>
2022-12-21 14:14:45 -0500

    updating db identifier

--- terraform/preview/locals.tf
+++ terraform/preview/locals.tf
@@ -13,7 +13,7 @@ locals {
       service_name      = "preview-hubzone-webmap"
       service_shortname = "preview-webmap"
       ecr_name          = "hubzone-webmap-preview"
-      db_identifier     = "preview-hubzone-aurora"
+      db_identifier     = "preview-hubzone"
       log_bucket        = "${local.account_id}-logs"
 
       rails_port        = 3000
