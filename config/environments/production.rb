198e73994d52807cd24b40f0ae59a9910f1ee4a8
nchudiwale <nchudiwale@oasysic.com>
2023-11-01 12:16:42 -0400

    fix AllState in dropdown, fix Existing layer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  #config.assets.js_compressor = :uglifier
   config.assets.js_compressor = nil
   config.assets.css_compressor = :sass
 
